5b7fa8f69d3764b4bedd25b64e5199acf3306b85
Christopher Jensen <christopher.jensen@daugherty.com>
2019-05-03 23:43:28 -0500

    Looping Through a List of Books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,6 +4,9 @@
     <h1>
       {{ title }}
     </h1>
+    <ul>
+      <li v-for="book in books">{{ book.title}}: {{ book.author}}</li>
+    </ul>
   </div>
 
 </template>
@@ -14,7 +17,12 @@
     name: "BookList",
     data() {
       return {
-        title: "All Books"
+        title: "All Books",
+        books: [
+          { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+          { title: 'American Gods', author: 'Neil Gaiman' },
+          { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
+        ]
       };
     }
   };
@@ -25,6 +33,16 @@
 
   h1, h2 {
     font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: block;
+    margin: 0 10px;
   }
 
 </style>
